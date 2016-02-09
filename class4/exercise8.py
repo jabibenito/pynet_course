8bdbeab1d30a7576c68c597f597427ccd9138d0c
linkdebian <jbenito@batz.es>
2016-02-09 16:01:18 +0100

    Use Netmiko to change the logging buffer size (logging buffered <size>) and to disable console logging (no logging console) from a file on both pynet-rtr1 and pynet-rtr2 (see 'Errata and Other Info, item #4).

--- class4/exercise8.py
+++ class4/exercise8.py
@@ -1,3 +1,5 @@
+# Use Netmiko to change the logging buffer size (logging buffered <size>) and to disable console logging (no logging console)
+# from a file on both pynet-rtr1 and pynet-rtr2 (see 'Errata and Other Info, item #4).
 from getpass import getpass
 from netmiko import ConnectHandler
 
