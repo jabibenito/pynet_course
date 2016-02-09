98241fab2fe21442d0bb23e49a7a2ecc330e6289
linkdebian <jbenito@batz.es>
2016-02-09 11:35:56 +0100

    Execute show ip int brief with Pexpect

---
+++ class4/exercise3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import pexpect
+import sys
+from getpass import getpass
+
+def main():
+    ip_addr = '50.76.53.27'
+    username = 'pyclass'
+    port = 8022
+    password = getpass()
+
+    # ssh -l pyclass 50.76.53.27 -p 8022
+    ssh_conn = pexpect.spawn('ssh -l {} {} -p {}'.format(username, ip_addr, port))
+
+    # Useful for debugging the session
+    # ssh_conn.logfile = sys.stdout
+    ssh_conn.timeout = 3
+    ssh_conn.expect('ssword:')
+    ssh_conn.sendline(password)
+
+    ssh_conn.expect('#')
+
+    ssh_conn.sendline('show ip int brief')
+    ssh_conn.expect('#')
+
+    # Print the output of the show ip int brief command.
+    print ssh_conn.before
+
+if __name__ == "__main__":
+    main()
