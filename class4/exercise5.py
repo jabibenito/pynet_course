fba8a553a5083cd46c677d1d9ceafe352528b045
linkdebian <jbenito@batz.es>
2016-02-09 11:36:38 +0100

    Basics of Netmiko

---
+++ class4/exercise5.py
@@ -0,0 +1,18 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+import time
+
+password = getpass()
+
+pynet_rtr2 = {'device_type': 'cisco_ios', 'ip': '50.76.53.27', 'username': 'pyclass', 'password': password, 'port': 8022}
+
+ssh_connection = ConnectHandler(**pynet_rtr2)
+time.sleep(2)
+
+ssh_connection.config_mode()
+output = ssh_connection.find_prompt()
+
+print "The current state of the prompt is %s" % output
+
+
+
